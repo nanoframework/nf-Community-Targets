6e77ebfb6c1fa15e93b34605a213cb99ed89caec
piwi1263 <piwi1263@gmail.com>
2017-08-18 19:37:51 +0200

    Initial addition of two new boards
    - Add the STM32F411RE stock board
    - Add the IngenuityMicro Oxygen board
    
    Signed-off-by: piwi1263 <piwi1263@gmail.com>

---
+++ CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/nanoCLR/main.c
@@ -0,0 +1,63 @@
+//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <ch.h>
+#include <hal.h>
+#include <cmsis_os.h>
+
+#include "usbcfg.h"
+#include <WireProtocol_ReceiverThread.h>
+#include <nanoCLR_Application.h>
+#include <nanoPAL_BlockStorage.h>
+
+// need to declare the Receiver thread here
+osThreadDef(ReceiverThread, osPriorityNormal, 2048, "ReceiverThread");
+
+//  Application entry point.
+int main(void) {
+
+  // HAL initialization, this also initializes the configured device drivers
+  // and performs the board-specific initializations.
+  halInit();
+
+  // The kernel is initialized but not started yet, this means that
+  // main() is executing with absolute priority but interrupts are already enabled.
+  osKernelInitialize();
+
+  //  Initializes a serial-over-USB CDC driver.
+  sduObjectInit(&SDU1);
+  sduStart(&SDU1, &serusbcfg);
+
+  // Activates the USB driver and then the USB bus pull-up on D+.
+  // Note, a delay is inserted in order to not have to disconnect the cable after a reset
+  usbDisconnectBus(serusbcfg.usbp);
+  chThdSleepMilliseconds(1500);
+  usbStart(serusbcfg.usbp, &usbcfg);
+  usbConnectBus(serusbcfg.usbp);
+
+  // create the receiver thread
+  osThreadCreate(osThread(ReceiverThread), NULL);
+
+  // start kernel, after this main() will behave like a thread with priority osPriorityNormal
+  osKernelStart();
+
+  // preparation for the CLR startup
+  BlockStorage_AddDevices();
+
+  CLR_SETTINGS clrSettings;
+
+  memset(&clrSettings, 0, sizeof(CLR_SETTINGS));
+
+  clrSettings.MaxContextSwitches         = 50;
+  clrSettings.WaitForDebugger            = false;
+  clrSettings.EnterDebuggerLoopAfterExit = true;
+
+  // startup CLR now
+  ClrStartup(clrSettings);
+
+  while (true) { 
+    osDelay(100);
+  }
+}
