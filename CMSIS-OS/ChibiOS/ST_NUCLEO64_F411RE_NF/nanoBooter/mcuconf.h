0eacdb83aec5dd4407532645ba76f0176c6bbb3c
piwi1263 <piwi1263@gmail.com>
2018-01-31 01:55:13 +0100

    CMake updates for community board
    Changes made to have the community board up-to-date with latest cmake and features.
    
    Signed-off-by: piwi1263 <piwi1263@gmail.com>
    
    #ST_NUCLEO64_F411RE_NF#

--- CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/nanoBooter/mcuconf.h
+++ CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/nanoBooter/mcuconf.h
@@ -36,9 +36,9 @@
 #define STM32_SW                            STM32_SW_PLL
 #define STM32_PLLSRC                        STM32_PLLSRC_HSI
 #define STM32_PLLM_VALUE                    16
-#define STM32_PLLN_VALUE                    384
-#define STM32_PLLP_VALUE                    4
-#define STM32_PLLQ_VALUE                    8
+#define STM32_PLLN_VALUE                    192
+#define STM32_PLLP_VALUE                    2
+#define STM32_PLLQ_VALUE                    4
 
 #define STM32_HPRE                          STM32_HPRE_DIV1
 #define STM32_PPRE1                         STM32_PPRE1_DIV4
@@ -240,11 +240,11 @@
  * SERIAL driver system settings.
  */
 #define STM32_SERIAL_USE_USART1             FALSE
-#define STM32_SERIAL_USE_USART2             TRUE
+#define STM32_SERIAL_USE_USART2             FALSE
 #define STM32_SERIAL_USE_USART3             FALSE
 #define STM32_SERIAL_USE_UART4              FALSE
 #define STM32_SERIAL_USE_UART5              FALSE
-#define STM32_SERIAL_USE_USART6             FALSE
+#define STM32_SERIAL_USE_USART6             TRUE
 #define STM32_SERIAL_USART1_PRIORITY        12
 #define STM32_SERIAL_USART2_PRIORITY        12
 #define STM32_SERIAL_USART3_PRIORITY        12
@@ -317,11 +317,11 @@
  * USB driver system settings.
  */
 #define STM32_USB_USE_OTG1                  TRUE
-//#define STM32_USB_USE_OTG2                  FALSE
+#define STM32_USB_USE_OTG2                  FALSE
 #define STM32_USB_OTG1_IRQ_PRIORITY         14
-//#define STM32_USB_OTG2_IRQ_PRIORITY         14
+#define STM32_USB_OTG2_IRQ_PRIORITY         14
 #define STM32_USB_OTG1_RX_FIFO_SIZE         512
-//#define STM32_USB_OTG2_RX_FIFO_SIZE         1024
+#define STM32_USB_OTG2_RX_FIFO_SIZE         1024
 #define STM32_USB_OTG_THREAD_PRIO           LOWPRIO
 #define STM32_USB_OTG_THREAD_STACK_SIZE     128
 #define STM32_USB_OTGFIFO_FILL_BASEPRI      0
