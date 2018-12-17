b3154cfa388ce8d7d878544560a9a7129e91dc8b
José Simões <jose.simoes@eclo.solutions>
2018-12-17 11:50:40 +0000

    Add I2C config file for all targets
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>
    
    #ALL#

--- CMSIS-OS/ChibiOS/GHI_FEZ_CERB40_NF/target_windows_devices_i2c_config.cpp
+++ CMSIS-OS/ChibiOS/GHI_FEZ_CERB40_NF/target_windows_devices_i2c_config.cpp
@@ -3,6 +3,28 @@
 // See LICENSE file in the project root for full license information.
 //
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// THIS FILE IS BLANK ON PURPOSE BECAUSE THIS TARGET DOESN'T REQUIRE THIS SPECIFIC CONFIGURATION //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+#include "win_dev_i2c_native.h"
+
+//////////
+// I2C1 //
+//////////
+
+// pin configuration for I2C1
+// port for SCL pin is: GPIOB
+// port for SDA pin is: GPIOB
+// SCL pin: is GPIOB_8
+// SDA pin: is GPIOB_9
+// GPIO alternate pin function is 4 (see alternate function mapping table in device datasheet)
+I2C_CONFIG_PINS(1, GPIOB, GPIOB, 8, 9, 4)
+
+//////////
+// I2C2 //
+//////////
+
+// pin configuration for I2C2
+// port for SCL pin is: GPIOB
+// port for SDA pin is: GPIOB
+// SCL pin: is GPIOB_10
+// SDA pin: is GPIOB_11
+// GPIO alternate pin function is 4 (see alternate function mapping table in device datasheet)
+I2C_CONFIG_PINS(2, GPIOB, GPIOB, 10, 11, 4)
