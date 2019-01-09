2eb20b83909c56f703d4d1fd128df1d43e90ce62
Vishnu Sharma <26829246+sharmavishnu@users.noreply.github.com>
2019-01-09 09:51:11 -0800

    Enable and config 1-Wire for ST_NUCLEO64_F401RE_NF (#75)
    #ST_NUCLEO64_F401RE_NF#

---
+++ CMSIS-OS/ChibiOS/ST_NUCLEO64_F401RE_NF/target_nf_devices_onewire_config.h
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+///////////
+// UART1 //
+///////////
+
+// enable USART1
+#define NF_ONEWIRE_STM32_UART_USE_USART1    TRUE
+
+// buffers size
+// tx buffer size: 32 bytes
+#define UART1_TX_SIZE  32
+// rx buffer size: 32 bytes
+#define UART1_RX_SIZE  32
