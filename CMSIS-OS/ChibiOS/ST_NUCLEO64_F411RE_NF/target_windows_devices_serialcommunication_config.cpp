08ec13830a100466d1097cee5064a80f6fee0370
Peter Wessel <piwi1263@gmail.com>
2019-05-04 17:13:00 +0200

    Update target_windows_devices_serialcommunication_config.cpp

--- CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/target_windows_devices_serialcommunication_config.cpp
+++ CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/target_windows_devices_serialcommunication_config.cpp
@@ -7,15 +7,15 @@
 #include "win_dev_serial_native.h"
 
 ///////////
-// UART2 //
+// UART6 //
 ///////////
 
-// pin configuration for UART2
+// pin configuration for UART6
 // port: GPIOA
-// TX pin: is GPIOA_2
-// RX pin: is GPIOA_3
+// TX pin: is GPIOA_11
+// RX pin: is GPIOA_12
 // GPIO alternate pin function is 7 (see "Table 9. Alternate function mapping" in STM32F411xC and STM32F411xE datasheet)
-UART_CONFIG_PINS(2, GPIOA, GPIOA, 2, 3, 7)
+UART_CONFIG_PINS(6, GPIOA, GPIOA, 11, 12, 8)
 
 // buffers
 // buffers that are R/W by DMA are recommended to be aligned with 32 bytes cache page size boundary
@@ -23,14 +23,14 @@ UART_CONFIG_PINS(2, GPIOA, GPIOA, 2, 3, 7)
 #if defined(__GNUC__)
 __attribute__((aligned (32)))
 #endif
-uint8_t Uart2_TxBuffer[UART2_TX_SIZE];
+uint8_t Uart6_TxBuffer[UART6_TX_SIZE];
 #if defined(__GNUC__)
 __attribute__((aligned (32)))
 #endif
-uint8_t Uart2_RxBuffer[UART2_RX_SIZE];
+uint8_t Uart6_RxBuffer[UART6_RX_SIZE];
 
-// initialization for UART2
-UART_INIT(2, UART2_TX_SIZE, UART2_RX_SIZE)
+// initialization for UART6
+UART_INIT(6, UART6_TX_SIZE, UART6_RX_SIZE)
 
-// un-initialization for UART2
-UART_UNINIT(2)
+// un-initialization for UART6
+UART_UNINIT(6)
