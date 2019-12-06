8de1dd6b44c2a4055f2566c883972a1576643124
Peter Wessel <piwi1263@gmail.com>
2019-12-06 18:17:44 +0100

    Add-new-f411-discovery-board (#112)
    - Update CMakes to build new board target.
    - Update README.
    
    ***ST_STM32F411_DISCOVERY***
    
    Signed-off-by: piwi1263 <piwi1263@gmail.com>

---
+++ CMSIS-OS/ChibiOS/ST_STM32F411_DISCOVERY/managed_helpers/ST_NUCLEO64_F411RE_NF.Adc.cs
@@ -0,0 +1,58 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.Targets.ST_NUCLEO64_F411RE_NF
+{
+    /// <summary>
+    /// Enumeration of ADC channels available on ST_NUCLEO64_F411RE_NF
+    /// </summary>
+    public static class AdcChannels
+    {
+        /// <summary>
+        /// Channel 0, exposed as A0, connected to pin 1 on CN8 = PA0 (ADC1 - IN0)
+        /// </summary>
+        public const int Channel_0 = 0;
+
+        /// <summary>
+        /// Channel 1, exposed as A1, connected to pin 2 on CN8 = PA1 (ADC1 - IN1)
+        /// </summary>
+        public const int Channel_1 = 1;
+
+        /// <summary>
+        /// Channel 2, exposed as A2, connected to pin 3 on CN8 = PA4 (ADC1 - IN4)
+        /// </summary>
+        public const int Channel_2 = 2;
+
+        /// <summary>
+        /// Channel 3, exposed as A3, connected to pin 4 on CN8 = PB0 (ADC1 - IN8)
+        /// </summary>
+        public const int Channel_3 = 3;
+
+        /// <summary>
+        /// Channel 4, exposed as A4, connected to pin 5 on CN8 = PC1 (ADC1 - IN11)
+        /// </summary>
+        public const int Channel_4 = 4;
+
+        /// <summary>
+        /// Channel 5, exposed on A5, connected to pin 6 on CN8 = PC0 (ADC1 - IN10)
+        /// </summary>
+        public const int Channel_5 = 5;
+
+        /// <summary>
+        /// Channel 6, internal temperature sensor, connected to ADC1
+        /// </summary>
+        public const int Channel_TemperatureSensor = 6;
+
+        /// <summary>
+        /// Channel 7, internal voltage reference, connected to ADC1
+        /// </summary>
+        public const int Channel_VrefIn = 7;
+
+        /// <summary>
+        /// Channel 8, connected to VBatt pin, ADC1
+        /// </summary>
+        public const int Channel_Vbatt = 8;
+    }
+}
---
+++ CMSIS-OS/ChibiOS/ST_STM32F411_DISCOVERY/nanoBooter/main.c
@@ -0,0 +1,79 @@
+//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <ch.h>
+#include <hal.h>
+#include <cmsis_os.h>
+
+#include <serialcfg.h>
+#include <swo.h>
+#include <targetHAL.h>
+#include <WireProtocol_ReceiverThread.h>
+#include <nanoPAL_BlockStorage.h>
+#include <LaunchCLR.h>
+
+// need to declare the Receiver thread here
+osThreadDef(ReceiverThread, osPriorityHigh, 2048, "ReceiverThread");
+
+//  Application entry point.
+int main(void) {
+
+  // HAL initialization, this also initializes the configured device drivers
+  // and performs the board-specific initializations.
+  halInit();
+
+  // init SWO as soon as possible to make it available to output ASAP
+  #if (SWO_OUTPUT == TRUE)  
+  SwoInit();
+  #endif
+
+  // the following IF is not mandatory, it's just providing a way for a user to 'force'
+  // the board to remain in nanoBooter and not launching nanoCLR
+
+  // if the USER button (blue one) is pressed, skip the check for a valid CLR image and remain in booter
+  if (!palReadPad(GPIOA, GPIOA_BUTTON))
+  {
+    // check for valid CLR image 
+    if(CheckValidCLRImage((uint32_t)&__nanoImage_end__))
+    {
+      // there seems to be a valid CLR image
+      // launch nanoCLR
+      LaunchCLR((uint32_t)&__nanoImage_end__);
+    }
+  }
+
+  // The kernel is initialized but not started yet, this means that
+  // main() is executing with absolute priority but interrupts are already enabled.
+  osKernelInitialize();
+    
+  // starts the serial driver
+  sdStart(&SERIAL_DRIVER, NULL);
+
+  // create the receiver thread
+  osThreadCreate(osThread(ReceiverThread), NULL);
+
+  // start kernel, after this main() will behave like a thread with priority osPriorityNormal
+  osKernelStart();
+
+  // initialize block storage list and devices
+  // in CLR this is called in nanoHAL_Initialize()
+  // for nanoBooter we have to init it in order to provide the flash map for Monitor_FlashSectorMap command
+  BlockStorageList_Initialize();
+  BlockStorage_AddDevices();
+
+  //  Normal main() thread
+  while (true) {
+      palSetPad(GPIOD, GPIOD_LD4_GREEN);
+      palSetPad(GPIOD, GPIOD_LD3_ORANGE);
+      palSetPad(GPIOD, GPIOD_LD5_RED);
+      palSetPad(GPIOD, GPIOD_LD6_BLUE);
+      osDelay(500);      
+      palClearPad(GPIOD, GPIOD_LD4_GREEN);
+      palClearPad(GPIOD, GPIOD_LD3_ORANGE);
+      palClearPad(GPIOD, GPIOD_LD5_RED);
+      palClearPad(GPIOD, GPIOD_LD6_BLUE);
+      osDelay(500);
+  }
+}
