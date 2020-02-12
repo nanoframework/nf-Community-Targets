95dd341c51f0f885019ee79d25ee577f12b09661
José Simões <jose.simoes@eclo.solutions>
2020-02-12 09:50:55 +0000

    Update nF overlay configs (#115)

--- CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoCLR/halconf_nf.h
+++ CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoCLR/halconf_nf.h
@@ -7,8 +7,8 @@
 #define _HALCONF_NF_H_ 1
 
 // enables STM32 Flash driver
-#if !defined(HAL_USE_STM32_FLASH) 
-#define HAL_USE_STM32_FLASH         TRUE
+#if !defined(HAL_NF_USE_STM32_FLASH) 
+#define HAL_NF_USE_STM32_FLASH         TRUE
 #endif
 
 #endif // _HALCONF_NF_H_
