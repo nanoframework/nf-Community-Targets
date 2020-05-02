ef50bb30047cdfeadea841a89879fadec00e386a
josesimoes <jose.simoes@eclo.solutions>
2020-05-02 17:11:04 +0100

    Update SPI config on all  ST targets
    #ALL#
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/target_windows_devices_spi_config.cpp
+++ CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/target_windows_devices_spi_config.cpp
@@ -5,6 +5,14 @@
 
 #include "win_dev_spi_native_target.h"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// THIS FILE IS BLANK ON PURPOSE BECAUSE THIS TARGET DOESN'T REQUIRE THIS SPECIFIC CONFIGURATION //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+// SPI5 //
+//////////
+
+// pin configuration for SPI5
+// port for SCK pin is: SPI5_SCK
+// port for MISO pin is: SPI5_MISO
+// port for MOSI pin is: SPI5_MOSI
+
+// GPIO alternate pin function is 5 (see alternate function mapping table in device datasheet)
+SPI_CONFIG_PINS(5, GPIOF, 7, GPIOF, 8, GPIOF, 9, 5)
