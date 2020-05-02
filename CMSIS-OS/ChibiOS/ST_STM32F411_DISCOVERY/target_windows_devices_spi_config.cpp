894aeecc52557acce771ddc90ca0d97fe0d2e892
josesimoes <jose.simoes@eclo.solutions>
2020-05-02 17:11:04 +0100

    Update SPI config on all  ST targets
    #ALL#
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- CMSIS-OS/ChibiOS/ST_STM32F411_DISCOVERY/target_windows_devices_spi_config.cpp
+++ CMSIS-OS/ChibiOS/ST_STM32F411_DISCOVERY/target_windows_devices_spi_config.cpp
@@ -5,6 +5,26 @@
 
 #include "win_dev_spi_native_target.h"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// THIS FILE IS BLANK ON PURPOSE BECAUSE THIS TARGET DOESN'T REQUIRE THIS SPECIFIC CONFIGURATION //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+// SPI1 //
+//////////
+
+// pin configuration for SPI1
+// port for SCK pin is: SPI1_SCLK
+// port for MISO pin is: SPI1_MISO
+// port for MOSI pin is: SPI1_MOSI
+
+// GPIO alternate pin function is 5 (see alternate function mapping table in device datasheet)
+SPI_CONFIG_PINS(2, GPIOA, 5, GPIOA, 6, GPIOA, 7, 5)
+
+//////////
+// SPI2 //
+//////////
+
+// pin configuration for SPI2
+// port for SCK pin is: SPI2_SCLK
+// port for MISO pin is: SPI2_MISO
+// port for MOSI pin is: SPI2_MOSI
+
+// GPIO alternate pin function is 5 (see alternate function mapping table in device datasheet)
+SPI_CONFIG_PINS(2, GPIOB, 13, GPIOB, 14, GPIOB, 15, 5)
