1ffcc310c8f7482f954eef96be0166c552903f13
Valon M Hoti <valon.hoti@gmail.com>
2020-05-04 10:11:15 +0200

    fix  - configuration for SPI3
    fix  - configuration for SPI3

--- CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/target_windows_devices_spi_config.cpp
+++ CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/target_windows_devices_spi_config.cpp
@@ -27,4 +27,4 @@ SPI_CONFIG_PINS(2, GPIOB, 13, GPIOB, 14, GPIOB, 15, 5)
 // port for MOSI pin is: SPI3_MOSI
 
 // GPIO alternate pin function is 5 (see alternate function mapping table in device datasheet)
-SPI_CONFIG_PINS(2, GPIOC, 10, GPIOC, 11, GPIOC, 12, 5)
+SPI_CONFIG_PINS(3, GPIOC, 10, GPIOC, 11, GPIOC, 12, 5)
