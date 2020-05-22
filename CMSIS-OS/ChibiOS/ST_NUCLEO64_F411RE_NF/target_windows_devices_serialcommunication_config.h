418534146a5689c5e488a5f54ea525710a373188
piwi1263 <piwi1263@gmail.com>
2020-05-22 20:23:25 +0200

    Update serial configuration and change OEM String
    * Change default use of USB on PA11, PA12 to serial use of USART6
    * Change default use of PA15, PB7 to use USART1
    * Update serial config to use USART1
    * Update the OEM String
    
    ***ST_NUCLEO64_F411RE_NF***

--- CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/target_windows_devices_serialcommunication_config.h
+++ CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/target_windows_devices_serialcommunication_config.h
@@ -5,6 +5,19 @@
 
 
 ///////////
+// UART1 //
+///////////
+
+// enable USART1
+#define NF_SERIAL_COMM_STM32_UART_USE_USART1    TRUE
+
+// buffers size 
+// tx buffer size: 1024 bytes
+#define UART1_TX_SIZE  1024
+// rx buffer size: 1024 bytes
+#define UART1_RX_SIZE  1024
+
+///////////
 // UART6 //
 ///////////
 
