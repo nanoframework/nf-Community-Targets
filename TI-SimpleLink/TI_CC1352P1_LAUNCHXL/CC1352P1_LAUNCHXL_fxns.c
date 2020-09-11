6422ca0fb0ac59b7e757528bd89d0a5c728b0275
José Simões <jose.simoes@eclo.solutions>
2020-09-11 14:39:03 +0100

    Update TI_CC1352P1_LAUNCHXL (#136)
    ***TI_CC1352P1_LAUNCHXL***

--- TI-SimpleLink/TI_CC1352P1_LAUNCHXL/CC1352P1_LAUNCHXL_fxns.c
+++ TI-SimpleLink/TI_CC1352P1_LAUNCHXL/CC1352P1_LAUNCHXL_fxns.c
@@ -42,12 +42,9 @@
 
 #include <ti/devices/cc13x2_cc26x2/driverlib/ioc.h>
 #include <ti/devices/cc13x2_cc26x2/driverlib/cpu.h>
-#include <ti/drivers/rf/RF.h>
 #include <ti/drivers/pin/PINCC26XX.h>
 
 #include <ti/drivers/Board.h>
-
-#include "Board.h"
 
 /*
  *  ======== CC1352P1_LAUNCHXL_sendExtFlashByte ========
@@ -147,136 +144,10 @@ void CC1352P1_LAUNCHXL_shutDownExtFlash(void)
 }
 
 /*
- *  For the SysConfig generated Board.h file, Board_RF_SUB1GHZ will not be
- *  defined unless the RF module is added to the configuration.  Therefore,
- *  we don't include this code if Board_RF_SUB1GHZ is not defined.
- */
-#if defined(Board_RF_SUB1GHZ)
-
-/*
- * ======== Antenna switching ========
- */
-static PIN_Handle antennaPins;
-static PIN_State antennaState;
-
-void initAntennaSwitch()
-{
-    PIN_Config antennaConfig[] = {
-        Board_RF_24GHZ | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,      /* Path disabled */
-        Board_RF_HIGH_PA | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,    /* Path disabled */
-        Board_RF_SUB1GHZ | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,    /* Path disabled */
-        PIN_TERMINATE
-    };
-    antennaPins = PIN_open(&antennaState, antennaConfig);
-}
-
-/*
- * ======== CC1352P1_LAUNCHXL_rfDriverCallback ========
- * Sets up the antenna switch depending on the current PHY configuration.
- * Truth table:
- *
- * Path        DIO28 DIO29 DIO30
- * =========== ===== ===== =====
- * Off         0     0     0
- * Sub-1 GHz   0     0     1
- * 2.4 GHz     1     0     0
- * 20 dBm TX   0     1     0
- */
-void rfDriverCallback(RF_Handle client, RF_GlobalEvent events, void *arg)
-{
-    /* Local variable. */
-    bool    sub1GHz   = false;
-    uint8_t loDivider = 0;
-
-    /* Switch off all paths first. Needs to be done anyway in every sub-case below. */
-    PINCC26XX_setOutputValue(Board_RF_24GHZ, 0);
-    PINCC26XX_setOutputValue(Board_RF_HIGH_PA, 0);
-    PINCC26XX_setOutputValue(Board_RF_SUB1GHZ, 0);
-
-    if (events & RF_GlobalEventRadioSetup) {
-        /* Decode the current PA configuration. */
-        RF_TxPowerTable_PAType paType = (RF_TxPowerTable_PAType)RF_getTxPower(client).paType;
-
-        /* Decode the generic argument as a setup command. */
-        RF_RadioSetup* setupCommand = (RF_RadioSetup*)arg;
-
-        switch (setupCommand->common.commandNo) {
-            case (CMD_RADIO_SETUP):
-            case (CMD_BLE5_RADIO_SETUP):
-                    loDivider = RF_LODIVIDER_MASK & setupCommand->common.loDivider;
-
-                    /* Sub-1GHz front-end. */
-                    if (loDivider != 0) {
-                        sub1GHz = true;
-                    }
-                    break;
-            case (CMD_PROP_RADIO_DIV_SETUP):
-                    loDivider = RF_LODIVIDER_MASK & setupCommand->prop_div.loDivider;
-
-                    /* Sub-1GHz front-end. */
-                    if (loDivider != 0) {
-                        sub1GHz = true;
-                    }
-                    break;
-            default:break;
-        }
-
-        if (sub1GHz) {
-            /* Sub-1 GHz */
-            if (paType == RF_TxPowerTable_HighPA) {
-                /* PA enable --> HIGH PA
-                 * LNA enable --> Sub-1 GHz
-                 */
-                PINCC26XX_setMux(antennaPins, Board_RF_24GHZ, PINCC26XX_MUX_GPIO);
-                /* Note: RFC_GPO3 is a work-around because the RFC_GPO1 (PA enable signal) is sometimes not
-                         de-asserted on CC1352 Rev A. */
-                PINCC26XX_setMux(antennaPins, Board_RF_HIGH_PA, PINCC26XX_MUX_RFC_GPO3);
-                PINCC26XX_setMux(antennaPins, Board_RF_SUB1GHZ, PINCC26XX_MUX_RFC_GPO0);
-            } else {
-                /* RF core active --> Sub-1 GHz */
-                PINCC26XX_setMux(antennaPins, Board_RF_24GHZ, PINCC26XX_MUX_GPIO);
-                PINCC26XX_setMux(antennaPins, Board_RF_HIGH_PA, PINCC26XX_MUX_GPIO);
-                PINCC26XX_setMux(antennaPins, Board_RF_SUB1GHZ, PINCC26XX_MUX_GPIO);
-                PINCC26XX_setOutputValue(Board_RF_SUB1GHZ, 1);
-            }
-        } else {
-            /* 2.4 GHz */
-            if (paType == RF_TxPowerTable_HighPA)
-            {
-                /* PA enable --> HIGH PA
-                 * LNA enable --> 2.4 GHz
-                 */
-                PINCC26XX_setMux(antennaPins, Board_RF_24GHZ, PINCC26XX_MUX_RFC_GPO0);
-                /* Note: RFC_GPO3 is a work-around because the RFC_GPO1 (PA enable signal) is sometimes not
-                         de-asserted on CC1352 Rev A. */
-                PINCC26XX_setMux(antennaPins, Board_RF_HIGH_PA, PINCC26XX_MUX_RFC_GPO3);
-                PINCC26XX_setMux(antennaPins, Board_RF_SUB1GHZ, PINCC26XX_MUX_GPIO);
-            } else {
-                /* RF core active --> 2.4 GHz */
-                PINCC26XX_setMux(antennaPins, Board_RF_24GHZ, PINCC26XX_MUX_GPIO);
-                PINCC26XX_setMux(antennaPins, Board_RF_HIGH_PA, PINCC26XX_MUX_GPIO);
-                PINCC26XX_setMux(antennaPins, Board_RF_SUB1GHZ, PINCC26XX_MUX_GPIO);
-                PINCC26XX_setOutputValue(Board_RF_24GHZ, 1);
-            }
-        }
-    } else {
-        /* Reset the IO multiplexer to GPIO functionality */
-        PINCC26XX_setMux(antennaPins, Board_RF_24GHZ, PINCC26XX_MUX_GPIO);
-        PINCC26XX_setMux(antennaPins, Board_RF_HIGH_PA, PINCC26XX_MUX_GPIO);
-        PINCC26XX_setMux(antennaPins, Board_RF_SUB1GHZ, PINCC26XX_MUX_GPIO);
-    }
-}
-#endif
-
-/*
  *  ======== Board_initHook ========
  *  Called by Board_init() to perform board-specific initialization.
  */
 void Board_initHook()
 {
-#if defined(Board_RF_SUB1GHZ)
-    initAntennaSwitch();
-#endif
-
     CC1352P1_LAUNCHXL_shutDownExtFlash();
 }
