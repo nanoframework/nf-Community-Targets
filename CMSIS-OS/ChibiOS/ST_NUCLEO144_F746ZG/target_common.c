9983c02db27da9dd6e2a0b096c880f42db106ee9
José Simões <jose.simoes@eclo.solutions>
2020-11-06 12:08:41 +0000

    Update device capabilities (#141)
    - Build is failing for all STM32 targets as they require update to ChibiOS 20.3
    
    ***TI_CC1352P1_LAUNCHXL***

--- CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/target_common.c
+++ CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/target_common.c
@@ -4,28 +4,25 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#include <nanoHAL_v2.h>
-#include "target_board.h"
 #include "target_common.h"
+#include "target_board.h"
+#include <nanoHAL_v2.h>
 #include <platform_target_capabilities.h>
 
-HAL_SYSTEM_CONFIG HalSystemConfig =
-{
-    { true }, // HAL_DRIVER_CONFIG_HEADER Header;
 
-    1,  //ConvertCOM_DebugHandle(1),
-    0,  //ConvertCOM_DebugHandle(0),
+HAL_SYSTEM_CONFIG HalSystemConfig = {
+    {true}, // HAL_DRIVER_CONFIG_HEADER Header;
+
+    1, // ConvertCOM_DebugHandle(1),
+    0, // ConvertCOM_DebugHandle(0),
     921600,
-    0,  // STDIO = COM2 or COM1
+    0, // STDIO = COM2 or COM1
 
-    { RAM1_MEMORY_StartAddress, RAM1_MEMORY_Size },
-    { FLASH1_MEMORY_StartAddress, FLASH1_MEMORY_Size }
-};
+    {RAM1_MEMORY_StartAddress, RAM1_MEMORY_Size},
+    {FLASH1_MEMORY_StartAddress, FLASH1_MEMORY_Size}};
 
-HAL_TARGET_CONFIGURATION  g_TargetConfiguration;
+HAL_TARGET_CONFIGURATION g_TargetConfiguration;
 
-// this target use DFU for updates
-uint32_t GetTargetCapabilities()
-{ 
-    return TargetCapabilities_DfuUpdate; 
-};
+// this target can use both JTAG and DFU for updates
+inline GET_TARGET_CAPABILITIES(TargetCapabilities_JtagUpdate |
+                               TargetCapabilities_DfuUpdate);
