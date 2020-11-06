92e79aa5ddcfede58aec3042effcd359d5d9b7d5
josesimoes <jose.simoes@eclo.solutions>
2020-11-06 12:02:53 +0000

    Update device capabilities
    - Following nanoframework/nf-interpreter#1758.

--- CMSIS-OS/ChibiOS/BrainPad2/target_common.c
+++ CMSIS-OS/ChibiOS/BrainPad2/target_common.c
@@ -4,28 +4,24 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#include <nanoHAL_v2.h>
-#include "target_board.h"
 #include "target_common.h"
+#include "target_board.h"
+#include <nanoHAL_v2.h>
 #include <platform_target_capabilities.h>
 
-HAL_SYSTEM_CONFIG HalSystemConfig =
-{
-    { true }, // HAL_DRIVER_CONFIG_HEADER Header;
 
-    1,  //ConvertCOM_DebugHandle(1),
-    0,  //ConvertCOM_DebugHandle(0),
+HAL_SYSTEM_CONFIG HalSystemConfig = {
+    {true}, // HAL_DRIVER_CONFIG_HEADER Header;
+
+    1, // ConvertCOM_DebugHandle(1),
+    0, // ConvertCOM_DebugHandle(0),
     921600,
-    0,  // STDIO = COM2 or COM1
+    0, // STDIO = COM2 or COM1
 
-    { RAM1_MEMORY_StartAddress, RAM1_MEMORY_Size },
-    { FLASH1_MEMORY_StartAddress, FLASH1_MEMORY_Size }
-};
+    {RAM1_MEMORY_StartAddress, RAM1_MEMORY_Size},
+    {FLASH1_MEMORY_StartAddress, FLASH1_MEMORY_Size}};
 
-HAL_TARGET_CONFIGURATION  g_TargetConfiguration;
+HAL_TARGET_CONFIGURATION g_TargetConfiguration;
 
 // this target uses DFU for updates
-uint32_t GetTargetCapabilities()
-{ 
-    return TargetCapabilities_DfuUpdate; 
-};
+GET_TARGET_CAPABILITIES(TargetCapabilities_DfuUpdate)
