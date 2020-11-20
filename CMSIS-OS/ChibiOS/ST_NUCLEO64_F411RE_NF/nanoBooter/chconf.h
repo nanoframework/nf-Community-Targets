918c7fb64982b210c7eb7542f50c25d618e95187
Valon M Hoti <valon.hoti@gmail.com>
2020-11-20 08:29:43 +0100

    Update chconf.h

--- CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/nanoBooter/chconf.h
+++ CMSIS-OS/ChibiOS/ST_NUCLEO64_F411RE_NF/nanoBooter/chconf.h
@@ -19,7 +19,7 @@
 #define CHCONF_H
 
 #define _CHIBIOS_RT_CONF_
-#define _CHIBIOS_RT_CONF_VER_6_0_
+#define _CHIBIOS_RT_CONF_VER_6_1_
 
 /*===========================================================================*/
 /**
