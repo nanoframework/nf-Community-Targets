391c8cd887de34b23447269a145b467e9d249c1c
José Simões <jose.simoes@eclo.solutions>
2021-04-01 12:51:09 +0100

    Move ChibiOS folder up (#161)
    ***NO_CI***

---
+++ ChibiOS/BrainPad2/managed_helpers/Buzzer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using Windows.Devices.Pwm;
+
+namespace FirmwareSelectors
+{
+    class Buzzer
+    {
+        private PwmController controller;
+
+        private PwmPin buzz;
+
+        public Buzzer()
+        {
+            this.controller = PwmController.FromId("TIM4"); 
+            this.buzz = controller.OpenPin(24);
+        }
+
+        public void Beep()
+        {
+            this.StartBuzzing(2500);
+            Thread.Sleep(20);
+            this.StopBuzzing();
+        }
+    
+        public void StartBuzzing(double frequency)
+        {
+            this.StopBuzzing();
+            if (frequency > 0)
+            {
+                this.controller.SetDesiredFrequency(frequency);
+                this.buzz.Start();
+                this.buzz.SetActiveDutyCyclePercentage(0.5);
+            }
+        }
+
+        public void StopBuzzing()
+        {
+            this.buzz.Stop();
+        }
+
+   
+    }
+}
