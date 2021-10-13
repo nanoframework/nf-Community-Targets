355d20fce2c13583beae4555a2007833cc78dad4
Robin Jones <networkfusion@users.noreply.github.com>
2021-10-13 19:10:14 +0100

    Update ports_config.h files

--- ChibiOS/GHI_FEZ_CERB40_NF/target_system_io_ports_config.h
+++ ChibiOS/GHI_FEZ_CERB40_NF/target_system_io_ports_config.h
@@ -1,4 +1,32 @@
 //
-// Copyright (c) 2017 The nanoFramework project contributors
+// Copyright (c) .NET Foundation and Contributors
 // See LICENSE file in the project root for full license information.
 //
+
+
+///////////
+// UART1 //
+///////////
+
+// enable USART1
+#define NF_SERIAL_COMM_STM32_UART_USE_USART1    TRUE
+
+// buffers size
+// tx buffer size: 256 bytes
+#define UART1_TX_SIZE  256
+// rx buffer size: 256 bytes
+#define UART1_RX_SIZE  256
+
+
+///////////
+// UART2 //
+///////////
+
+// enable USART2
+#define NF_SERIAL_COMM_STM32_UART_USE_USART2    TRUE
+
+// buffers size
+// tx buffer size: 1024 bytes
+#define UART2_TX_SIZE  1024
+// rx buffer size: 1024 bytes
+#define UART2_RX_SIZE  1024
--- ChibiOS/PybStick2x/target_system_io_ports_config.h
+++ ChibiOS/PybStick2x/target_system_io_ports_config.h
@@ -1,4 +1,18 @@
 //
-// Copyright (c) 2017 The nanoFramework project contributors
+// Copyright (c) .NET Foundation and Contributors
 // See LICENSE file in the project root for full license information.
 //
+
+
+///////////
+// UART2 //
+///////////
+
+// enable USART2
+#define NF_SERIAL_COMM_STM32_UART_USE_USART2   TRUE
+
+// buffers size
+// tx buffer size: 1024 bytes
+#define UART2_TX_SIZE  1024
+// rx buffer size: 1024 bytes
+#define UART2_RX_SIZE  1024
