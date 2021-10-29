a98a728cd9c3c6e1ff2bd2d60823f32eef78cf70
José Simões <jose.simoes@eclo.solutions>
2021-10-29 17:53:09 +0100

    Add support for System.Device.Adc
    - Add new config file to various targets.
    - Ported changes from upstream in W.D.Adc.
    - Add new build options to Azure pipeline yaml.

--- ChibiOS/I2M_ELECTRON_NF/target_system_device_adc_config.cpp
+++ ChibiOS/I2M_ELECTRON_NF/target_system_device_adc_config.cpp
@@ -2,19 +2,15 @@
 // Copyright (c) .NET Foundation and Contributors
 // See LICENSE file in the project root for full license information.
 //
+ 
+#include <sys_dev_adc_native_target.h>
 
-#include <win_dev_adc_native_target.h>
 
+const NF_PAL_ADC_PORT_PIN_CHANNEL AdcPortPinConfig[] = {
 
-const NF_PAL_ADC_PORT_PIN_CHANNEL__ AdcPortPinConfig__[] = {
-    
     // ADC1
-    {1, GPIOA, 0, ADC_CHANNEL_IN0},
-    {1, GPIOA, 1, ADC_CHANNEL_IN1},
     {1, GPIOA, 4, ADC_CHANNEL_IN4},
-    {1, GPIOB, 0, ADC_CHANNEL_IN8},
-    {1, GPIOC, 1, ADC_CHANNEL_IN11},
-    {1, GPIOC, 0, ADC_CHANNEL_IN10},
+    {1, GPIOA, 5, ADC_CHANNEL_IN5},
 
     // these are the internal sources, available only at ADC1
     {1, NULL, 0, ADC_CHANNEL_SENSOR},
@@ -22,4 +18,4 @@ const NF_PAL_ADC_PORT_PIN_CHANNEL__ AdcPortPinConfig__[] = {
     {1, NULL, 0, ADC_CHANNEL_VBAT},
 };
 
-const int AdcChannelCount__ = ARRAYSIZE(AdcPortPinConfig__);
+const int AdcChannelCount = ARRAYSIZE(AdcPortPinConfig);
