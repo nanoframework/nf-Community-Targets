89e9c32dc77d8c6ed13eda018bb2dd8e01673a42
Robin Jones <robin@networkfusion.co.uk>
2021-12-08 18:05:37 +0000

    Ignore clang format for HAL files. Makes it easier to compare on updates.

--- ChibiOS/ST_NUCLEO144_F412ZG_NF/nanoCLR/halconf.h
+++ ChibiOS/ST_NUCLEO144_F412ZG_NF/nanoCLR/halconf.h
@@ -1,3 +1,4 @@
+// clang-format off
 //
 // Copyright (c) .NET Foundation and Contributors
 // Portions Copyright (c) 2006..2015 Giovanni Di Sirio.  All rights reserved.
@@ -548,3 +549,4 @@
 #endif /* HALCONF_H */
 
 /** @} */
+// clang-format on
