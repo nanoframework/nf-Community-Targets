570ab8adea6fe6962b9660c5ec63cfebc136f875
José Simões <jose.simoes@eclo.solutions>
2021-12-10 12:50:08 +0000

    Simplification of WP HAL Interface (#189)
    ***NO_CI***

--- ChibiOS/GHI_FEZ_CERB40_NF/nanoCLR/main.c
+++ ChibiOS/GHI_FEZ_CERB40_NF/nanoCLR/main.c
@@ -15,66 +15,68 @@
 #include <nanoCLR_Application.h>
 #include <nanoPAL_BlockStorage.h>
 #include <nanoHAL_v2.h>
-#include <targetPAL.h> 
+#include <targetPAL.h>
 
 // need to declare the Receiver thread here
 osThreadDef(ReceiverThread, osPriorityHigh, 2048, "ReceiverThread");
-// declare CLRStartup thread here 
-osThreadDef(CLRStartupThread, osPriorityNormal, 4096, "CLRStartupThread"); 
+// declare CLRStartup thread here
+osThreadDef(CLRStartupThread, osPriorityNormal, 4096, "CLRStartupThread");
 
 //  Application entry point.
-int main(void) {
+int main(void)
+{
 
-  // HAL initialization, this also initializes the configured device drivers
-  // and performs the board-specific initializations.
-  halInit();
+    // HAL initialization, this also initializes the configured device drivers
+    // and performs the board-specific initializations.
+    halInit();
 
-  // init SWO as soon as possible to make it available to output ASAP
-  #if (SWO_OUTPUT == TRUE)  
-  SwoInit();
-  #endif
+// init SWO as soon as possible to make it available to output ASAP
+#if (SWO_OUTPUT == TRUE)
+    SwoInit();
+#endif
 
-  // The kernel is initialized but not started yet, this means that
-  // main() is executing with absolute priority but interrupts are already enabled.
-  osKernelInitialize();
+    // The kernel is initialized but not started yet, this means that
+    // main() is executing with absolute priority but interrupts are already enabled.
+    osKernelInitialize();
 
-  // start watchdog
-  Watchdog_Init();
+    // start watchdog
+    Watchdog_Init();
 
-  #if (HAL_NF_USE_STM32_CRC == TRUE)
-  // startup crc
-  crcStart(NULL);
-  #endif
+#if (HAL_NF_USE_STM32_CRC == TRUE)
+    // startup crc
+    crcStart(NULL);
+#endif
 
-  //  Initializes a serial-over-USB CDC driver.
-  sduObjectInit(&SDU1);
-  sduStart(&SDU1, &serusbcfg);
+    //  Initializes a serial-over-USB CDC driver.
+    sduObjectInit(&SERIAL_DRIVER);
+    sduStart(&SERIAL_DRIVER, &serusbcfg);
 
-  // Activates the USB driver and then the USB bus pull-up on D+.
-  // Note, a delay is inserted in order to not have to disconnect the cable after a reset
-  usbDisconnectBus(serusbcfg.usbp);
-  chThdSleepMilliseconds(100);
-  usbStart(serusbcfg.usbp, &usbcfg);
-  usbConnectBus(serusbcfg.usbp);
+    // Activates the USB driver and then the USB bus pull-up on D+.
+    // Note, a delay is inserted in order to not have to disconnect the cable after a reset
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(100);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp);
 
-  // create the receiver thread
-  osThreadCreate(osThread(ReceiverThread), NULL);
+    // create the receiver thread
+    osThreadCreate(osThread(ReceiverThread), NULL);
 
-  // CLR settings to launch CLR thread
-  CLR_SETTINGS clrSettings;
-  (void)memset(&clrSettings, 0, sizeof(CLR_SETTINGS));
+    // CLR settings to launch CLR thread
+    CLR_SETTINGS clrSettings;
+    (void)memset(&clrSettings, 0, sizeof(CLR_SETTINGS));
 
-  clrSettings.MaxContextSwitches         = 50;
-  clrSettings.WaitForDebugger            = false;
-  clrSettings.EnterDebuggerLoopAfterExit = true;
+    clrSettings.MaxContextSwitches = 50;
+    clrSettings.WaitForDebugger = false;
+    clrSettings.EnterDebuggerLoopAfterExit = true;
 
-  // create the CLR Startup thread 
-  osThreadCreate(osThread(CLRStartupThread), &clrSettings);
+    // create the CLR Startup thread
+    osThreadCreate(osThread(CLRStartupThread), &clrSettings);
 
-  // start kernel, after this main() will behave like a thread with priority osPriorityNormal
-  osKernelStart();
+    // start kernel, after this main() will behave like a thread with priority osPriorityNormal
+    osKernelStart();
 
-  while (true) { 
-    osDelay(100);
-  }
+    while (true)
+    {
+        osDelay(100);
+    }
 }
