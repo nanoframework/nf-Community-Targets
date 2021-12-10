f31dbc4138f6624f58a3ee724a63193e6c3ff166
José Simões <jose.simoes@eclo.solutions>
2021-12-10 12:32:41 +0000

    Simplification of WP HAL Interface
    - Following nanoframework/nf-interpreter#2172.

--- ChibiOS/BrainPad2/nanoBooter/main.c
+++ ChibiOS/BrainPad2/nanoBooter/main.c
@@ -5,7 +5,7 @@
 
 #include <ch.h>
 #include <hal.h>
-#include <hal_nf_community.h>			
+#include <hal_nf_community.h>
 #include <cmsis_os.h>
 
 #include <usbcfg.h>
@@ -18,57 +18,59 @@
 osThreadDef(ReceiverThread, osPriorityHigh, 2048, "ReceiverThread");
 
 //  Application entry point.
-int main(void) {
+int main(void)
+{
 
-  // HAL initialization, this also initializes the configured device drivers
-  // and performs the board-specific initializations.
-  halInit();
+    // HAL initialization, this also initializes the configured device drivers
+    // and performs the board-specific initializations.
+    halInit();
 
-  // check for valid CLR image at address contiguous to nanoBooter
-  // this target DOES NOT have configuration block, so we need to use the __nanoImage_end__ address here
-    if(CheckValidCLRImage((uint32_t)&__nanoImage_end__))
-  {
-    // there seems to be a valid CLR image
-    // launch nanoCLR
-    LaunchCLR((uint32_t)&__nanoImage_end__);
-  }
-    
-  // The kernel is initialized but not started yet, this means that
-  // main() is executing with absolute priority but interrupts are already enabled.
-  osKernelInitialize();
+    // check for valid CLR image at address contiguous to nanoBooter
+    // this target DOES NOT have configuration block, so we need to use the __nanoImage_end__ address here
+    if (CheckValidCLRImage((uint32_t)&__nanoImage_end__))
+    {
+        // there seems to be a valid CLR image
+        // launch nanoCLR
+        LaunchCLR((uint32_t)&__nanoImage_end__);
+    }
 
-  #if (HAL_NF_USE_STM32_CRC == TRUE)
-  // startup crc
-  crcStart(NULL);
-  #endif	
-  //  Initializes a serial-over-USB CDC driver.
-  sduObjectInit(&SDU1);
-  sduStart(&SDU1, &serusbcfg);
+    // The kernel is initialized but not started yet, this means that
+    // main() is executing with absolute priority but interrupts are already enabled.
+    osKernelInitialize();
 
-  // Activates the USB driver and then the USB bus pull-up on D+.
-  // Note, a delay is inserted in order to not have to disconnect the cable after a reset.
-  usbDisconnectBus(serusbcfg.usbp);
-  chThdSleepMilliseconds(1500);
-  usbStart(serusbcfg.usbp, &usbcfg);
-  usbConnectBus(serusbcfg.usbp);
+#if (HAL_NF_USE_STM32_CRC == TRUE)
+    // startup crc
+    crcStart(NULL);
+#endif
+    //  Initializes a serial-over-USB CDC driver.
+    sduObjectInit(&SERIAL_DRIVER);
+    sduStart(&SERIAL_DRIVER, &serusbcfg);
 
-  // create the receiver thread
-  osThreadCreate(osThread(ReceiverThread), NULL);
+    // Activates the USB driver and then the USB bus pull-up on D+.
+    // Note, a delay is inserted in order to not have to disconnect the cable after a reset.
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(1500);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp);
 
-  // start kernel, after this main() will behave like a thread with priority osPriorityNormal
-  osKernelStart();
+    // create the receiver thread
+    osThreadCreate(osThread(ReceiverThread), NULL);
 
-  // initialize block storage list and devices
-  // in CLR this is called in nanoHAL_Initialize()
-  // for nanoBooter we have to init it in order to provide the flash map for Monitor_FlashSectorMap command
-  BlockStorageList_Initialize();
-  BlockStorage_AddDevices();
+    // start kernel, after this main() will behave like a thread with priority osPriorityNormal
+    osKernelStart();
 
-  //  Normal main() thread
-  while (true) {
-      palSetPad(GPIOA, GPIOA_USER_LED);
-      osDelay(500);      
-      palClearPad(GPIOA, GPIOA_USER_LED);
-      osDelay(500);
-  }
+    // initialize block storage list and devices
+    // in CLR this is called in nanoHAL_Initialize()
+    // for nanoBooter we have to init it in order to provide the flash map for Monitor_FlashSectorMap command
+    BlockStorageList_Initialize();
+    BlockStorage_AddDevices();
+
+    //  Normal main() thread
+    while (true)
+    {
+        palSetPad(GPIOA, GPIOA_USER_LED);
+        osDelay(500);
+        palClearPad(GPIOA, GPIOA_USER_LED);
+        osDelay(500);
+    }
 }
--- ChibiOS/I2M_ELECTRON_NF/nanoBooter/main.c
+++ ChibiOS/I2M_ELECTRON_NF/nanoBooter/main.c
@@ -19,63 +19,65 @@
 osThreadDef(ReceiverThread, osPriorityHigh, 2048, "ReceiverThread");
 
 //  Application entry point.
-int main(void) {
+int main(void)
+{
 
-  // HAL initialization, this also initializes the configured device drivers
-  // and performs the board-specific initializations.
-  halInit();
+    // HAL initialization, this also initializes the configured device drivers
+    // and performs the board-specific initializations.
+    halInit();
 
-  // init SWO as soon as possible to make it available to output ASAP
-  #if (SWO_OUTPUT == TRUE)  
-  SwoInit();
-  #endif
+// init SWO as soon as possible to make it available to output ASAP
+#if (SWO_OUTPUT == TRUE)
+    SwoInit();
+#endif
 
-  // The kernel is initialized but not started yet, this means that
-  // main() is executing with absolute priority but interrupts are already enabled.
-  osKernelInitialize();
-  osDelay(20);    // Let init stabilize
+    // The kernel is initialized but not started yet, this means that
+    // main() is executing with absolute priority but interrupts are already enabled.
+    osKernelInitialize();
+    osDelay(20); // Let init stabilize
 
-  // check for valid CLR image 
-  if(CheckValidCLRImage((uint32_t)&__nanoImage_end__))
-  {
-    // there seems to be a valid CLR image
-    // launch nanoCLR
-    LaunchCLR((uint32_t)&__nanoImage_end__);
-  }
+    // check for valid CLR image
+    if (CheckValidCLRImage((uint32_t)&__nanoImage_end__))
+    {
+        // there seems to be a valid CLR image
+        // launch nanoCLR
+        LaunchCLR((uint32_t)&__nanoImage_end__);
+    }
 
-  #if (HAL_NF_USE_STM32_CRC == TRUE)
-  // startup crc
-  crcStart(NULL);
-  #endif
-  
-  //  Initializes a serial-over-USB CDC driver.
-  sduObjectInit(&SDU1);
-  sduStart(&SDU1, &serusbcfg);
+#if (HAL_NF_USE_STM32_CRC == TRUE)
+    // startup crc
+    crcStart(NULL);
+#endif
 
-  // Activates the USB driver and then the USB bus pull-up on D+.
-  // Note, a delay is inserted in order to not have to disconnect the cable after a reset.
-  usbDisconnectBus(serusbcfg.usbp);
-  chThdSleepMilliseconds(100);
-  usbStart(serusbcfg.usbp, &usbcfg);
-  usbConnectBus(serusbcfg.usbp);
+    //  Initializes a serial-over-USB CDC driver.
+    sduObjectInit(&SERIAL_DRIVER);
+    sduStart(&SERIAL_DRIVER, &serusbcfg);
 
-  // create the receiver thread
-  osThreadCreate(osThread(ReceiverThread), NULL);
+    // Activates the USB driver and then the USB bus pull-up on D+.
+    // Note, a delay is inserted in order to not have to disconnect the cable after a reset.
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(100);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp);
 
-  // start kernel, after this main() will behave like a thread with priority osPriorityNormal
-  osKernelStart();
+    // create the receiver thread
+    osThreadCreate(osThread(ReceiverThread), NULL);
 
-  // initialize block storage list and devices
-  // in CLR this is called in nanoHAL_Initialize()
-  // for nanoBooter we have to init it in order to provide the flash map for Monitor_FlashSectorMap command
-  BlockStorageList_Initialize();
-  BlockStorage_AddDevices();
+    // start kernel, after this main() will behave like a thread with priority osPriorityNormal
+    osKernelStart();
 
-  //  Normal main() thread
-  while (true) {
-      palSetPad(GPIOA, GPIOA_LED_GREEN);
-      osDelay(500);      
-      palClearPad(GPIOA, GPIOA_LED_GREEN);
-      osDelay(500);
-  }
+    // initialize block storage list and devices
+    // in CLR this is called in nanoHAL_Initialize()
+    // for nanoBooter we have to init it in order to provide the flash map for Monitor_FlashSectorMap command
+    BlockStorageList_Initialize();
+    BlockStorage_AddDevices();
+
+    //  Normal main() thread
+    while (true)
+    {
+        palSetPad(GPIOA, GPIOA_LED_GREEN);
+        osDelay(500);
+        palClearPad(GPIOA, GPIOA_LED_GREEN);
+        osDelay(500);
+    }
 }
