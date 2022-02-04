6b0eed5890c6ed1637322f130510f50dde7b26a9
José Simões <jose.simoes@eclo.solutions>
2022-02-04 08:46:45 +0000

    Fix build for TI targets (#199)

--- TI_SimpleLink/TI_CC1352P1_LAUNCHXL/nanoCLR/main.c
+++ TI_SimpleLink/TI_CC1352P1_LAUNCHXL/nanoCLR/main.c
@@ -3,7 +3,6 @@
 // See LICENSE file in the project root for full license information.
 //
 
-
 #include <stdint.h>
 #include <nanoCLR_Application.h>
 #include <nanoHAL_v2.h>
@@ -26,7 +25,7 @@
 //////////////////////////////
 
 // Stack size in bytes
-#define THREADSTACKSIZE   2048
+#define THREADSTACKSIZE 2048
 
 Task_Handle receiverHandle;
 Task_Handle clrHandle;
@@ -39,14 +38,6 @@ CLR_SETTINGS clrSettings;
 // these are declared in cpu_gpio.cpp
 extern GPIO_PinConfig gpioPinConfigs[GPIO_MAX_PINS];
 extern GPIO_CallbackFxn gpioCallbackFunctions[GPIO_MAX_PINS];
-
-// this has to be define in a C file, otherwise the linker can't replace the weak one declared in the SDK driver library
-const GPIOCC26XX_Config GPIOCC26XX_config = {
-    .pinConfigs = (GPIO_PinConfig *)gpioPinConfigs,
-    .callbacks = (GPIO_CallbackFxn *)gpioCallbackFunctions,
-    .numberOfPinConfigs = GPIO_MAX_PINS,
-    .numberOfCallbacks = GPIO_MAX_PINS,
-    .intPriority = (~0)};
 
 extern void ReceiverThread(UArg arg0, UArg arg1);
 extern void CLRStartupThread(UArg arg0, UArg arg1);
@@ -71,14 +62,15 @@ int main(void)
     receiverHandle = Task_create((Task_FuncPtr)ReceiverThread, &taskParams, Error_IGNORE);
     if (receiverHandle == NULL)
     {
-        while (1);
+        while (1)
+            ;
     }
 
-    // CLR settings to launch CLR thread   
+    // CLR settings to launch CLR thread
     (void)memset(&clrSettings, 0, sizeof(CLR_SETTINGS));
 
-    clrSettings.MaxContextSwitches         = 50;
-    clrSettings.WaitForDebugger            = false;
+    clrSettings.MaxContextSwitches = 50;
+    clrSettings.WaitForDebugger = false;
     clrSettings.EnterDebuggerLoopAfterExit = true;
 
     // setup CLR task
@@ -88,13 +80,14 @@ int main(void)
     clrHandle = Task_create(CLRStartupThread, &taskParams, Error_IGNORE);
     if (clrHandle == NULL)
     {
-        while (1);
+        while (1)
+            ;
     }
 
     GPIO_init();
     ADC_init();
     ConfigUART();
-    
+
     BIOS_start();
 
     return (0);
