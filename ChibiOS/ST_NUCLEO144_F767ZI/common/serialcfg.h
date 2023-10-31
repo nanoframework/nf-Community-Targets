b44874c1003e34fee5809fb7fa80ea2c6b2ffd7d
Robin Jones <networkfusion@users.noreply.github.com>
2023-10-31 00:13:22 +0000

    Add ST_NUCLEO144_F767ZI

---
+++ ChibiOS/ST_NUCLEO144_F767ZI/common/serialcfg.h
@@ -0,0 +1,18 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+// clang-format off
+
+#ifndef SERIALCFG_H
+#define SERIALCFG_H
+
+// define which serial driver the Wire Protocol will be using 
+#define SERIAL_DRIVER           SD3
+
+#endif /* SERIALCFG_H */
+
+/** @} */
+
+// clang-format on
---
+++ ChibiOS/ST_NUCLEO144_F767ZI/managed_helpers/README.md
@@ -0,0 +1,5 @@
+## Managed code helpers
+
+This folder contains helper classes to make developers life easier when coding for this target board.
+
+* [Adc](STM32F767ZI_NUCLEO.Adc.cs)
