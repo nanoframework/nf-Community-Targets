3573555268e946c01dcd62554191781feec9635a
José Simões <jose.simoes@eclo.solutions>
2026-06-03 11:43:17 +0100

    Fix USB string declarations

--- ChibiOS/ST_STM32F4_DISCOVERY/common/usbcfg.c
+++ ChibiOS/ST_STM32F4_DISCOVERY/common/usbcfg.c
@@ -35,7 +35,8 @@ typedef struct usb_string_vendor
 {
     uint8_t bLength;
     uint8_t bDescriptorType;
-    wchar_t bPropertyData[sizeof(USB_STRING_VENDOR)/sizeof(wchar_t) - 1];  // that's the 'w_char' string length less 1 because we are not storing the terminator (according to the USB spec)
+    // includes the C terminator; the USB descriptor length below excludes it
+    wchar_t bPropertyData[sizeof(USB_STRING_VENDOR) / sizeof(wchar_t)];
 
 }usb_string_vendor;
 
@@ -50,7 +51,8 @@ typedef struct usb_string_device_description
 {
     uint8_t bLength;
     uint8_t bDescriptorType;
-    wchar_t bPropertyData[sizeof(USB_STRING_DEVICE_DESCRIPTION)/sizeof(wchar_t) - 1];  // that's the 'w_char' string length less 1 because we are not storing the terminator (according to the USB spec)
+    // includes the C terminator; the USB descriptor length below excludes it
+    wchar_t bPropertyData[sizeof(USB_STRING_DEVICE_DESCRIPTION) / sizeof(wchar_t)];
 
 }usb_string_device_description;
 
@@ -59,7 +61,7 @@ typedef struct usb_string_device_description
 // device serial number
 // this will produce a string with NANO_ prefix followed 
 // by the hexadecimal representation of the silicon unique ID of the CPU
-#define USB_STRING_SERIAL_NUMBER      L"NANO_xxxxxxxxxxx"
+#define USB_STRING_SERIAL_NUMBER      L"NANO_xxxxxxxxxxxx"
 /////////////////////////////////////////////////////////////////////////
 #define INDEX_OF_WCHAR_FOR_UNIQUE_ID  5
 
@@ -68,7 +70,8 @@ typedef struct usb_string_serial_number
 {
     uint8_t bLength;
     uint8_t bDescriptorType;
-    wchar_t bPropertyData[sizeof(USB_STRING_SERIAL_NUMBER)/sizeof(wchar_t) - 1];  // that's the 'w_char' string length less 1 because we are not storing the terminator (according to the USB spec)
+    // includes the C terminator; the USB descriptor length below excludes it
+    wchar_t bPropertyData[sizeof(USB_STRING_SERIAL_NUMBER) / sizeof(wchar_t)];
 
 }usb_string_serial_number;
 
@@ -196,7 +199,7 @@ static const uint8_t vcom_string0[] = {
 
 // Vendor string
 static const usb_string_vendor usb_vendor = {
-  sizeof(usb_vendor),
+  sizeof(usb_vendor) - sizeof(wchar_t),
   USB_DESC_BYTE(USB_DESCRIPTOR_STRING),
   USB_STRING_VENDOR
 };
@@ -204,7 +207,7 @@ static const usb_string_vendor usb_vendor = {
 
 // Device Description string
 static const usb_string_device_description usb_device_description = {
-  sizeof(usb_device_description),
+  sizeof(usb_device_description) - sizeof(wchar_t),
   USB_DESC_BYTE(USB_DESCRIPTOR_STRING),
   USB_STRING_DEVICE_DESCRIPTION
 };
@@ -212,7 +215,7 @@ static const usb_string_device_description usb_device_description = {
 
 // Serial Number string.
 static usb_string_serial_number usb_serial_number = {
-  sizeof(usb_serial_number),
+  sizeof(usb_serial_number) - sizeof(wchar_t),
   USB_DESC_BYTE(USB_DESCRIPTOR_STRING),
   USB_STRING_SERIAL_NUMBER
 };
@@ -223,9 +226,9 @@ static usb_string_serial_number usb_serial_number = {
  */
 static const USBDescriptor vcom_strings[] = {
   {sizeof vcom_string0, vcom_string0},
-  {sizeof usb_vendor, (uint8_t*)(&usb_vendor)},
-  {sizeof usb_device_description, (uint8_t*)(&usb_device_description)},
-  {sizeof usb_serial_number, (uint8_t*)(&usb_serial_number)},
+  {sizeof usb_vendor - sizeof(wchar_t), (uint8_t*)(&usb_vendor)},
+  {sizeof usb_device_description - sizeof(wchar_t), (uint8_t*)(&usb_device_description)},
+  {sizeof usb_serial_number - sizeof(wchar_t), (uint8_t*)(&usb_serial_number)},
 };
 
 
